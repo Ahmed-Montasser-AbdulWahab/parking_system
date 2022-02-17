2c0d06b3a5826d5b138d66ac02695f716ef44317
Ahmed Montasser AbdulWahab <1700205@eng.asu.edu.eg>
2022-02-17 07:32:55 +0200

    Adding Gate Class

---
+++ Classes/Gate.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Parking_System.Classes
+{
+    public class Gate
+    {
+        //ClosedState => 0 ,OpenedState => 1
+        private Boolean state = false;
+        public Boolean State { get { return state; } }
+
+        public Boolean Open()
+        {
+            state = true;
+            return this.state;
+        }
+
+        public Boolean Close()
+        {
+            state = false;
+            return this.state;
+        }
+    }
+}
