a9e16376c0e0c274c30eee4a4cc1aa9019772e8f
Ahmed Montasser AbdulWahab <1700205@eng.asu.edu.eg>
2022-02-17 09:05:44 +0200

    Adding ParkingArea Class

--- Classes/ParkingArea.cs
+++ Classes/ParkingArea.cs
@@ -8,5 +8,45 @@ namespace Parking_System.Classes
 {
     public class ParkingArea
     {
+        private string _name;
+
+        public ParkingArea(string name)
+        {
+              _name = name;
+        }
+
+        /*
+         public void listenFromTerminal(){}
+        */
+
+        public Boolean CheckPairing()
+        {
+            return true;
+        }
+
+        public Boolean CheckRegistered()
+        {
+            return true;
+        }
+
+        public ParkingTransaction GenerateTransaction(string cID, int terminalID, Boolean result, DateTime dateTime)
+        {
+            return new ParkingTransaction(cID, terminalID, result, dateTime);
+        }
+
+        public void TransformToOperator()
+        {
+
+        }
+
+        public double ComputeBill()
+        {
+            return 1.5;
+        }
+
+        public void Activate()
+        {
+
+        }
     }
 }
