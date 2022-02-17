e01bc2ff6329d2ab5b96d791ecd2c24f8fc3e994
Ahmed Montasser AbdulWahab <1700205@eng.asu.edu.eg>
2022-02-17 07:53:19 +0200

    Adding Participant Class

--- Classes/Participant.cs
+++ Classes/Participant.cs
@@ -6,7 +6,20 @@ using System.Threading.Tasks;
 
 namespace Parking_System.Classes
 {
-    internal class Participant
+    public class Participant
     {
+        private int _participantID;
+        private string _participantName;
+        private string _faceID;
+
+        public Participant(int id, string name, string f_id)
+        {
+            _participantID = id;
+            _participantName = name;
+            _faceID = f_id;
+        }
+        public int ParticipantID { get { return _participantID; } }
+        public string ParticipantName { get { return _participantName; } }
+        public string FaceID { get { return _faceID; } set { _faceID = value; } }
     }
 }
